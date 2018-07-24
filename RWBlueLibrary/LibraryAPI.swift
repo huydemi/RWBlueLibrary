fa184c191f0ab0b509a2985022736efc94d2f74c
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-24 15:13:33 +0700

    How to Use the Singleton Pattern

---
+++ RWBlueLibrary/LibraryAPI.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAPI.swift
+//  RWBlueLibrary
+//
+//  Created by Dang Quoc Huy on 7/24/18.
+//  Copyright © 2018 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+final class LibraryAPI {
+  
+  static let shared = LibraryAPI()
+  
+  private init() {
+  }
+}
